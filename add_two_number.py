570cd88d0a767cd9ec2114aec8675f259edc0d29
JayWu16 <jaywu16@163.com>
2018-08-01 00:14:19 +0800

    modified

--- add_two_number.py
+++ add_two_number.py
@@ -1,4 +1,30 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     def addTwoNumbers(self, l1, l2):
-        
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+       """
+
+
+
+
+# node1 = ListNode(3)
+# node2 = ListNode(4)
+# node3 = ListNode(5)
+#
+# node1.next=node2
+# node2.next=node3
+#
+# node = node1
+# while True:
+#     print node.val
+#     if node.next:
+#         node = node.next
+#     else:
+#         break

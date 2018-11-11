bbe0e64d7864b0312556b1fc141408c939c90479
JayWu7 <34577397+JayWu7@users.noreply.github.com>
2018-11-11 12:34:53 +0800

    leetcode 7

---
+++ leetcode_7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverse(self, x):
+
+
+        str_x = str(x)
+        reverse_str_x = str_x[::-1]
+        reverse_int = 0
+        if (x >= 0):
+            reverse_int = int(reverse_str_x)
+        else:
+            reverse_int = -int(reverse_str_x[:-1])
+
+        if reverse_int >= (2 ** 31) or reverse_int < -(2 ** 31):
+            return 0
+        else:
+            return  reverse_int
+
+solution = Solution()
+print((2 ** 31) - 1)
+print(- (2 ** 31))
+print(solution.reverse(1534236469))

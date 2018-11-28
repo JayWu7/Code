700ef94f84088ea973a0264ea8ad2afd275e6c3c
JayWu7 <34577397+JayWu7@users.noreply.github.com>
2018-11-28 21:49:57 +0800

    Leetcode16
    罗马数字转整数

---
+++ leetcode_13.py
@@ -0,0 +1,35 @@
+class Solution:
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        specials = {'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
+        number = 0
+        index = 0
+
+        while index < len(s):
+            sp = s[index:index + 2]
+            if sp in specials:
+                number += specials[sp]
+                index += 2
+            else:
+                number += values[s[index]]
+                index += 1
+
+
+        return number
+
+# excellent code
+# class Solution:
+#     def romanToInt(self, s):
+#         """
+#         :type s: str
+#         :rtype: int
+#         """
+#         h = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+#         r, p = 0, 'I'
+#         for c in s[::-1]:
+#             r, p = r + h[c] if h[c] >= h[p] else r - h[c], c
+#         return r

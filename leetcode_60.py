36ada0c2b91f69cdccb7d9b603bba15d2c3c63d9
JayWu7 <34577397+JayWu7@users.noreply.github.com>
2018-12-04 20:22:37 +0800

    leetcode_60
    第k个排列

---
+++ leetcode_60.py
@@ -0,0 +1,36 @@
+class Solution:
+    def get_factorial(self, num):
+        i, factorial = 1, 1
+        while i <= num:
+            factorial = factorial * i
+            i = i + 1
+        return factorial
+
+    def getPermutation(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: str
+        """
+        num = ''
+
+        def gener_permu(numbers, k):
+            nonlocal num
+            if len(numbers) == 1:
+                num = num + str(numbers[0])
+                return
+            factorial = self.get_factorial(len(numbers) - 1)  # 获得n-1的阶乘
+            sequence = k / factorial  # 判断当前位置应该是几
+            if sequence == int(sequence):  # sequence不是整数
+                sequence = sequence - 1
+            sequence = int(sequence)
+            str_n = numbers.pop(sequence)
+            num = num + str(str_n)
+            k = k - factorial * sequence
+            gener_permu(numbers, k)
+
+        gener_permu([i + 1 for i in range(n)], k)
+        return num
+
+s = Solution()
+print(s.getPermutation(4,9))

3e9ef908245789f6f31c6eb3001f0d8e866d296a
JayWu7 <34577397+JayWu7@users.noreply.github.com>
2018-12-13 11:12:02 +0800

    买卖股票的最佳时机II

---
+++ leetcode_122.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        max_proofit = 0
+        for i in range(len(prices) - 1):
+            if prices[i + 1] > prices[i]:
+                max_proofit = max_proofit + prices[i + 1] - prices[i]
+
+        return max_proofit

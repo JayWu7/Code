fe8fc4aa97cd085a1de366d9be25aa9baec9b4d9
JayWu7 <34577397+JayWu7@users.noreply.github.com>
2018-12-22 21:22:21 +0800

    Update leetcode_350.py

--- leetcode_350.py
+++ leetcode_350.py
@@ -41,5 +41,20 @@ class Solution:
         return result
 
 
+#method 3, 假设nums2很大，不能直接存入内存
+class Solution:
+    def intersect(self, nums1, nums2):
+        num_map = dict()
+        res = []
+        for i in nums1:
+            num_map[i] = (num_map[i] + 1 if i in num_map else 1)
+        # 模拟从磁盘中一个一个读取数据
+        for i in nums2:
+            if i in num_map:
+                num_map[i] -= 1
+                res.append(i)
+                if num_map[i] == 0:
+                    num_map.pop(i)
 
+        return res
 

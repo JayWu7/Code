fd159f597f7aca62be26d10a1065a47ca0ef92e2
JayWu7 <34577397+JayWu7@users.noreply.github.com>
2018-12-24 23:19:22 +0800

    两数之和

--- leetcode_1.py
+++ leetcode_1.py
@@ -1,3 +1,4 @@
+# method 1
 class Solution:
     def twoSum(self, nums, target):
         for i1 in range(len(nums)):
@@ -7,3 +8,40 @@ class Solution:
                 i2 = nums.index(n2)
                 if i1 != i2:
                     return [i1, i2]
+
+
+# method 2
+class Solution:
+    def twoSum(self, nums, target):
+        """
+                :type nums: List[int]
+                :type target: int
+                :rtype: List[int]
+        思路：
+            首先排除target/2 在nums里存在的情况
+            字典键存储target-num的值，值存储num的下标
+            然后遍历nums，如果num在dict_num里，返回排序后的num下标i和dict_num的num键对应的值
+        """
+        if target / 2 in nums and nums.count(target / 2) > 1:
+            x = nums.index(target / 2)
+            y = nums.index(target / 2, x + 1)
+            return [x, y]
+
+        dict_nums = {target - num: i for i, num in enumerate(nums)}
+        for i, num in enumerate(nums):
+            if num in dict_nums and i != dict_nums[num]:
+                return sorted([i, dict_nums[num]])
+
+#excellent code
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hash_map = dict()
+        for k, v in enumerate(nums):
+            if target - v in hash_map:
+                return [k, hash_map[target-v]]
+            hash_map[v] = k

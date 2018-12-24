3d0429cd7ef578124d07366a34a6d30ca85381a8
JayWu7 <34577397+JayWu7@users.noreply.github.com>
2018-12-24 23:34:08 +0800

    两个数组的交集 II

--- leetcode_350.py
+++ leetcode_350.py
@@ -3,7 +3,7 @@ class Solution:
     def intersect(self, nums1, nums2):
         """
         :type nums1: List[int]
-        :type nums2: List[int]
+        :type nums2: List[int] 
         :rtype: List[int]
         """
         nums1.sort()

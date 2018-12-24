62bba203867deead7317e4b3336234f2300e443a
JayWu7 <34577397+JayWu7@users.noreply.github.com>
2018-12-24 23:27:44 +0800

    正则表达式匹配

--- leetcode_10.py
+++ leetcode_10.py
@@ -1,10 +1,5 @@
 class Solution:
     def isMatch(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: bool
-        """
         import re
         pattern = re.compile(p)
         if pattern.match(s) == None:

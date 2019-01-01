419708be82522198345458f58bb04ddbd815c0c1
JayWu7 <34577397+JayWu7@users.noreply.github.com>
2019-01-01 20:52:02 +0800

    将有序数组转换为二叉搜索树

---
+++ leetcode_108.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        思路:  将数组中值作为根节点，中值左边的数构造左子树，右边的数构造右子树，重复这一过程
+        """
+        if not nums:
+            return None
+        mid = len(nums) / 2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid + 1:])
+        return root

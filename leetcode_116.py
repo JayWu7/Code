499623264c9b2ecf5afbfa45d50ae0b37171d687
JayWu7 <34577397+JayWu7@users.noreply.github.com>
2019-01-11 13:11:25 +0800

    每个节点的右向指针

---
+++ leetcode_116.py
@@ -0,0 +1,23 @@
+# Definition for binary tree with next pointer.
+class TreeLinkNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+        self.next = None
+
+
+class Solution:
+    # @param root, a tree link node
+    # @return nothing
+    def connect(self, root):
+        def helper(root):
+            if not root or not root.left:
+                return
+            root.left.next = root.right
+            if root.next:
+                root.right.next = root.next.left
+            helper(root.left)
+            helper(root.right)
+
+        helper(root)

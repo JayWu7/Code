4afe05ac1d6cdabcdffd58a110e83e4fb15a5b46
Wu Xiaobo <34577397+JayWu7@users.noreply.github.com>
2019-05-05 17:49:47 +0800

    【栈、设计】扁平化嵌套列表迭代器

---
+++ leetcode_341.py
@@ -0,0 +1,63 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+# class NestedInteger(object):
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class NestedIterator(object):
+    '''
+    思路：利用递归，遍历出此嵌套列表里的所有数据，本题的主要难点在理解上面的 NestedInterger 类。
+    time: 1 hour
+    '''
+
+    def __init__(self, nestedList):
+        """
+        Initialize your data structure here.
+        :type nestedList: List[NestedInteger]
+        """
+        self.stack = []
+        self.helper(nestedList)
+        self.stack.reverse()
+
+
+    def helper(self, nestedList):
+        for nest in nestedList:
+            if nest.isInteger():  # 仅包含一个数
+                self.stack.append(nest.getInteger())
+            else:
+                self.helper(nest.getList())
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        return self.stack.pop()
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return True if self.stack else False
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())

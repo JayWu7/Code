bddf7312db16e6efafef8b60b282c259d063908c
Wu Xiaobo <34577397+JayWu7@users.noreply.github.com>
2019-05-18 00:23:05 +0800

    Update leetcode_463.py

--- leetcode_463.py
+++ leetcode_463.py
@@ -1,9 +1,25 @@
 class Solution(object):
     def islandPerimeter(self, grid):
         """
-        思路：递归
+        思路：遍历,如果是1，则total_island + 1
+            如果下或者右有连接，则decrease + 1
+            最后返回  4*total_island - decrease*2
+        time: 60min
+        速度 544ms，击败百分之百
         :type grid: List[List[int]]
         :rtype: int
         """
-        grid = [[0] * (len(grid)+1) + grid] + [[0] + row for row in grid]
+        le = len(grid)
+        total_island = 0
+        decrease = 0
+        for i in range(le):
+            le_r = len(grid[i])
+            for j in range(le_r):
+                if grid[i][j]:
+                    total_island += 1
+                    if i < le - 1 and grid[i + 1][j]:
+                        decrease += 1
+                    if j < le_r - 1 and grid[i][j + 1]:
+                        decrease += 1
+        return total_island * 4 - decrease * 2
 

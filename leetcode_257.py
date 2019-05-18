2c7a042b7f1322ac00d0540470e959f627392b6e
Wu Xiaobo <34577397+JayWu7@users.noreply.github.com>
2019-05-18 11:34:16 +0800

    【树、深度优先搜索】

---
+++ leetcode_257.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def binaryTreePaths(self, root):
+        """
+        思路： 递归，dns
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        if not root:
+            return []
+        res = []
+
+        def recursive(node, path):
+            if not node.left and not node.right:
+                res.append(path + str(node.val))
+            else:
+                path = path + '{}->'.format(node.val)
+                if node.left:
+                    recursive(node.left, path)
+                if node.right:
+                    recursive(node.right, path)
+
+        recursive(root, '')
+        return res
